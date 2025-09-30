e12f215b973dcceef69a740e58f74d7f33061bc9
sagardhondge <sagardhondge56@gmail.com>
2025-09-30 20:27:12 +0530

    frontend start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <div>
       <nav>
         <Link to="/register">Register</Link> |{" "}
-        <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link> |{" "}
+        <Link to="/dashboard">Dashboard</Link>
       </nav>
 
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
