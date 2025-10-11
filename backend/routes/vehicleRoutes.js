4d4042e65c30af87bb997388e163179cc9529368
sagardhondge <sagardhondge56@gmail.com>
2025-10-11 19:46:16 +0530

    updated backend

---
+++ backend/routes/vehicleRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import { addVehicle, getVehicles, deleteVehicle } from "../controllers/vehicleController.js";
+
+const router = express.Router();
+
+router.route("/")
+  .get(protect, getVehicles)
+  .post(protect, addVehicle);
+
+router.route("/:id").delete(protect, deleteVehicle);
+
+export default router;
