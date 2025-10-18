4dfc6ee14a8836205f3356be94330e8222d0c908
sagardhondge <sagardhondge56@gmail.com>
2025-10-18 17:25:23 +0530

    updated files

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,1 +1,22 @@
- 
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import {
+  addExpense,
+  getExpenses,
+  getExpenseById,
+  updateExpense,
+  deleteExpense,
+} from "../controllers/expenseController.js";
+
+const router = express.Router();
+
+router.route("/")
+  .get(protect, getExpenses)
+  .post(protect, addExpense);
+
+router.route("/:id")
+  .get(protect, getExpenseById)
+  .put(protect, updateExpense)
+  .delete(protect, deleteExpense);
+
+export default router; 
--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,1 +1,15 @@
- 
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import { addVehicle, getVehicles, updateVehicle, deleteVehicle } from "../controllers/vehicleController.js";
+
+const router = express.Router();
+
+router.route("/")
+  .get(protect, getVehicles)
+  .post(protect, addVehicle);
+
+router.route("/:id")
+  .put(protect, updateVehicle)
+  .delete(protect, deleteVehicle);
+
+export default router; 
